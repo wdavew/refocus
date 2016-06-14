3c1557b3ab486c9ce1fe0a08cb080ed64a167439
dwilson <dwilson@crai.com>
2016-06-14 18:07:06 -0400

    add event filter for onDOM listener

--- js/background.js
+++ js/background.js
@@ -1,5 +1,6 @@
 // Called when the user navigates to a given url
+var urlPattern = 'facebook';	
 chrome.webNavigation.onDOMContentLoaded.addListener(function (details) {
 	console.log('Running Script');
-	});
+	}, {url: [{hostEquals: urlPattern}]});
 
