e03cf265ae2f3ad9233586ae7e88d34fafc4f0da
David Wilson <w.dave.w@gmail.com>
2016-06-15 01:47:28 -0400

    Rough implementation of timer
    This needs work; currently will always redirect. Need to modify the
    listener so that it will only redirect the page more than once if
    returning to it after a while.

--- js/background.js
+++ js/background.js
@@ -18,23 +18,54 @@ function createBlacklist(blackListArray) {
 	};
 }
 
+function getTabURL(tab) {
+	return chrome.tabs.query({
+		active: true,
+		currentWindow: true
+	}, function(tabs) {
+		return tabs[0].url;
+	});
+}
 // Delay page load and redirect to waiting page
-function delayPageLoad(tabId, delay) {
+function delayPageLoad(tabId, delay, destURL) {
 	var waitingURL = chrome.extension.getURL('delay.html');
+	setTimeout(function (tabId) {
+		chrome.tabs.update(tabId, {url: destURL});
+	}, 5000);	
 	chrome.tabs.update(tabId, {url: waitingURL});
 }
 
 
 // Called when the user navigates to a given url
-function main(pageArray) {
+function createUpdateListener(pageArray) {
 	var blackListedURL = createBlacklist(pageArray); 	
+	var i = 1;
 	chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {
-	var url = parseURL(changeInfo.url);	
-		if (blackListedURL(url)){
-			console.log('Blacklisted');
-			delayPageLoad(tabId, 10);	
+		var waitingURL = chrome.extension.getURL('delay.html');
+		var url = parseURL(changeInfo.url);
+		console.log(tab.url);	
+		if (blackListedURL(url) && i < 2) {
+			i = i + 1;	
+			delayPageLoad(tabId, 10, changeInfo.url);	
 		}		
 	});
 }
 
+// Called when the user navigates to a given url
+function createSelectListener(pageArray) {
+	var blackListedURL = createBlacklist(pageArray); 	
+	chrome.tabs.onActivated.addListener(function (activeInfo) {
+		var tabURL = getTabURL();
+		var url = parseURL(tabURL);
+		console.log(url);	
+		if (blackListedURL(url)){
+			delayPageLoad(activeInfo.tabId, 10, url);
+		}	
+	});
+}
+
+function main(pageArray) {
+	createUpdateListener(pageArray);
+//	createSelectListener(pageArray);
+}
 main([/facebook/, /homestarrunner/]);
